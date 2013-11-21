51d15187fd1bfdbec8c142295bfa3485a34c2615
Christopher Martin <chmartin@cern.ch>
2013-11-21 17:32:15 +0100

    NOT FOR PUBLIC physics model debug

--- python/SpinZeroStructure.py
+++ python/SpinZeroStructure.py
@@ -8,33 +8,35 @@ class SpinZeroHiggs(PhysicsModel):
         self.muAsPOI = False
         self.muFloating = False
         self.poiMap = []
-        self.pois = {}
         self.verbose = False
     def setModelBuilder(self, modelBuilder):
         PhysicsModel.setModelBuilder(self,modelBuilder)
         self.modelBuilder.doModelBOnly = False
+
+    def getYieldScale(self,bin,process):
+        "Split in production and decay, and call getHiggsSignalYieldScale; return 1 for backgrounds "
         
+        if not self.DC.isSignal[process]: return 1
+
+        if self.muFloating:
+            print "Process ", process, "will get norm r"
+            return "r"
+
+        else:
+            print "Process ", process, " will get norm 1"
+            return 1
+
     def setPhysicsOptions(self,physOptions):
         for po in physOptions:
             if po == "muAsPOI":
                 print "Will consider the signal strength as a parameter of interest"
                 self.muAsPOI = True
                 self.muFloating = True
-            if po == "muFloating":
+            elif po == "muFloating":
                 print "Will consider the signal strength as a floating parameter (as a paraeter of interest if --PO muAsPOI is specified, as a nuisance otherwise)"
                 self.muFloating = True
-            if po.startswith("verbose"):
-                self.verbose = True
-            if po.startswith("map="):
-                self.muFloating = True
-                (maplist,poi) = po.replace("map=","").split(":")
-                maps = maplist.split(",")
-                poiname = re.sub("\[.*","",poi)
-                if poiname not in self.pois:
-                    if self.verbose: print "Will create a var ",poiname," with factory ",poi
-                    self.pois[poiname] = poi
-                if self.verbose: print "Mapping ",poiname," to ",maps," patterns"
-                self.poiMap.append((poiname,maps))
+            else:
+                print "Option not understood"
             
     def doParametersOfInterest(self):
         """Create POI and other parameters, and define the POI set."""
@@ -42,17 +44,11 @@ class SpinZeroHiggs(PhysicsModel):
         poi = "CMS_zz4l_fg4"
 
         if self.muFloating:
-            if self.pois:
-                for ph,pf in self.pois.items():
-                    self.modelBuilder.doVar(pf)
-                    if self.muAsPOI:
-                        print 'Treating %(pn)s as a POI' %locals()
-                        poi += ','+pn
-            else:
-                self.modelBuilder.doVar("r[1,0,4]")
-                if self.muAsPOI:
-                    print 'Treating r as a POI'
-                    poi += ",r"
+            self.modelBuilder.doVar("r[1,0,4]")
+            if self.muAsPOI:
+                print 'Treating r as a POI'
+                poi += ",r"
+        
         self.modelBuilder.doSet("POI",poi)
         
 spinZeroHiggs = SpinZeroHiggs()
